7a0ed769c3889738c2841a85c357667cb956bfd2
Colinr1994 <colinr1994@gmail.com>
2020-07-13 21:30:39 -0700

    init and complete?

---
+++ src/main/java/cst438hw3b/Cst438hw3bApplication.java
@@ -0,0 +1,13 @@
+package cst438hw3b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cst438hw3bApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cst438hw3bApplication.class, args);
+	}
+
+}
---
+++ src/main/java/cst438hw3b/ReservationEventHandler.java
@@ -0,0 +1,19 @@
+package cst438hw3b;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+
+@RabbitListener(queues = "city-reservation-q1")
+public class ReservationEventHandler {
+
+	private static final Logger Log = 
+			LoggerFactory.getLogger(ReservationEventHandler.class);
+	
+	@RabbitHandler
+	public void recieve(String in) {
+		System.out.println(in);
+		Log.info(" [ReservationEventHanlder] Revieved'" + in + "'");
+	}
+}
